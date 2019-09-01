337a74ecd78860bc1b3af2356812ca3a54e00a41
Imran Paruk <imranparuk@live.com>
2019-09-01 17:18:45 +0200

    Initial commit

--- predecessors/scraper_python/src/scraper/__init__.py
+++
@@ -1,4 +0,0 @@
-from .meta.meta import MetaScraper as MetaScraper
-from .product.loader import ScraperLoader as ScraperLoader
-from .product.product import scrape_product_data
-from .site.site import SiteScraper as SiteScraper
