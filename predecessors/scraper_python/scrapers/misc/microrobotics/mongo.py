6f688a8d102d520ae2c6734f8ac0176b11ddfd3c
Imran Paruk <imranparuk@live.com>
2020-05-19 21:07:18 +0200

    refactor 2.0

--- predecessors/scraper_python/scrapers/misc/microrobotics/mongo.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-# pprint library is used to make the output look more pretty
-from pprint import pprint
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient("localhost")
-db=client.microrobotics
