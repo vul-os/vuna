056c1c20a6402fe60b617c19e7e744e59d451c89
imranparuk <imranparuk@live.com>
2020-12-31 03:20:08 +0200

    updates

--- predecessors/scraper_python/main.py
+++ predecessors/scraper_python/main.py
@@ -7,24 +7,24 @@ from flask import request
 
 app = Flask(__name__)
 
-running_spiders = []
-
 @app.route('/', methods = ['GET', 'POST', 'DELETE'])
 def run_spider():
+    """
+    /home/imran/.local/share/DBeaverData/workspace6/General/Scripts
+    https://stackoverflow.com/questions/44820119/how-to-use-multiple-service-accounts-with-gcloud
+    :return:
+    :rtype:
+    """
     spider_name = request.data.decode('utf-8')
+    spider_name = spider_name if spider_name else None
     print(f"Starting Spyder: {spider_name}")
-    if spider_name is not None and spider_name not in running_spiders:
-        running_spiders.append(spider_name)
-        print(f"Running Spiders: {running_spiders}")
-        subprocess.check_output(['scrapy', 'crawl', spider_name, "-o", "output.json"], cwd=f'./{spider_name}')
-        running_spiders.remove(spider_name)
+    if spider_name is not None:
+        subprocess.check_output(['scrapy', 'crawl', spider_name, "-o", "output.json"], cwd=f'./scrapers/{spider_name}')
         # with open("output.json") as items_file:
         #     print(items_file.read())
-
     return spider_name
 
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
-# https://stackoverflow.com/questions/44820119/how-to-use-multiple-service-accounts-with-gcloud
