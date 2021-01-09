728abad33df0fddc7023d2904038e27be0d0b81e
imranparuk <imranparuk@live.com>
2021-01-10 00:09:03 +0200

    Initialize project using Create React App

--- predecessors/scraper-go/internal/pkg/storage/storage.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-type FileStorage interface {
-	WriteData(data interface{}, filePath string) error
-	ReadData(filePath string) (interface{}, error)
-	GetLatestFiles(folderPrefix, textIn string) ([]string, error)
-	GetLatestFile(folderPrefix, textIn string) (string, error)
-}
