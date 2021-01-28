658738bc1b97ef7f2f4d35e5e62e15602e61f021
imranparuk <imranparuk@live.com>
2021-01-28 23:27:31 +0200

    updates

---
+++ predecessors/scraper-frontend/src/components/Loading/Loding.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loading = (props) => {
+  return (
+    <img
+      alt="Logo"
+      src="/static/logo.svg"
+      {...props}
+    />
+  );
+};
+
+export default Logo;
