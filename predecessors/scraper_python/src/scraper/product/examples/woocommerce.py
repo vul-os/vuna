4099b527c274b15c4029b5b84cbf7fe3f6fc0030
Imran Paruk <imranparuk@live.com>
2023-05-12 12:30:19 +0000

    updates 3

--- predecessors/scraper_python/src/scraper/product/examples/woocommerce.py
+++ predecessors/scraper_python/src/scraper/product/examples/woocommerce.py
@@ -62,6 +62,42 @@ class Scraper:
                 else self.scrape_product_without_variations(product_url, soup, product_name)
         return json.dumps(product_data)
 
+    def scrape_product_without_variations(self, product_url, soup, product_name):
+        """
+        Scrape a product without variations.
+
+        Args:
+            product_url (str): The URL of the product to be scraped.
+            soup (bs4.BeautifulSoup): A BeautifulSoup object representing the HTML of the product page.
+            product_name (str): The name of the product being scraped.
+
+        Returns:
+            dict: A dictionary representing the product with keys 'name', 'url', 'sku', 'price', 
+            'max_qty', 'attributes', and 'image_url'.
+
+        """
+        price = soup.find('span', {'class': 'woocommerce-Price-amount amount'}).text.strip()
+        if '-' in price:
+            price_range = [float(x.strip().replace('$', '').replace(',', '')) for x in price.split('-')]
+            price = sum(price_range) / len(price_range)
+        else:
+            price = float(price.replace('$', '').replace(',', ''))
+        sku = soup.find('span', {'class': 'sku'}).text.strip() if soup.find('span', {'class': 'sku'}) else ''
+        max_qty = soup.find('p', {'class': 'stock'}).text.strip() if soup.find('p', {'class': 'stock'}) else ''
+        if isinstance(max_qty, str):
+            max_qty = max_qty.replace('in', '').replace('stock', '').strip()
+        max_qty = int(max_qty) if max_qty.isdigit() else None
+        image_url = soup.find('div', {'class': 'woocommerce-product-gallery__image'}).find('img')['src']
+        return {
+            'name': product_name,
+            'url': product_url,
+            'sku': sku,
+            'price': price,
+            'max_qty': max_qty,
+            'attributes': [],
+            'image_url': image_url
+        }
+
     def scrape_product_with_variations(self, product_url, soup, product_name):
         """
         Scrape a product with multiple variants and return a list of product variants.
