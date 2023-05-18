f79963492294139b45a0cc1b9a9106a9a0b883e6
Imran Paruk <imranparuk@live.com>
2023-05-18 23:34:50 +0000

    .

--- predecessors/scraper_python/src/api/orchestrator.py
+++ predecessors/scraper_python/src/api/orchestrator.py
@@ -6,21 +6,21 @@ from src.storage.local import StorageUtilsLocal as StorageUtils
 class OrchestratorAPI:
     def __init__(self):
         pass
-    
-    def meta(self, request):
-        try:
+
+    # def meta(self, request):
+    #     try:
             
-        except Exception as exception:
-            return str(exception), 500
+    #     except Exception as exception:
+    #         return str(exception), 500
 
-    def site(self, request):
-        try:
+    # def site(self, request):
+    #     try:
 
-        except Exception as exception:
-            return str(exception), 500
+    #     except Exception as exception:
+    #         return str(exception), 500
 
-    def product(self, request):
-        try:
+    # def product(self, request):
+    #     try:
 
-        except Exception as ex:
-            return str(ex), 500
+    #     except Exception as ex:
+    #         return str(ex), 500
--- predecessors/scraper_python/src/orchestrator/orchestrate.py
+++ predecessors/scraper_python/src/orchestrator/orchestrate.py
@@ -1,6 +1,7 @@
+import requests
+from google.cloud import storage
 
-def get_urls_from_gcs_file(bucket_name, file_name):
-    client = storage.Client()
+def get_urls_from_gcs_file(client: storage.Client, bucket_name: str, file_name: str):
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(file_name)
     
@@ -14,3 +15,24 @@ def get_urls_from_gcs_file(bucket_name, file_name):
         urls.append(line)
     
     return urls
+
+def retrieve_proxys_from_file():
+    file_url = 'https://spys.me/proxy.txt'
+    # Send a GET request to retrieve the file contents
+    response = requests.get(file_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Get the text content of the response
+        file_content = response.text
+
+        # Split the file content by newlines
+        lines = file_content.split('\n')
+
+        # Extract the URLs from the lines
+        urls = [line.split()[1] for line in lines if line.startswith('Http proxy')]
+
+        return urls
+    else:
+        print('Failed to retrieve the file:', response.status_code)
+        return []
