f58bc6717f53b594970c3b9cdf74ed8fb68eb7f9
Imran Paruk <imranparuk@live.com>
2023-05-18 22:31:40 +0000

    .

--- predecessors/scraper_python/src/orchestrator/processor/cloudtasks.py
+++ predecessors/scraper_python/src/orchestrator/processor/cloudtasks.py
@@ -1,60 +1,40 @@
-import os
-import pandas as pd
-from google.cloud import tasks_v2
-from google.protobuf import timestamp_pb2
-from datetime import datetime, timedelta
-from abc import ABC, abstractmethod
+from google.cloud import storage, tasks
 
 
-class CloudTasksBatchProcessor(BatchProcessor):
-    def __init__(self, folder_path, chunk_size=10):
-        super().__init__(folder_path, chunk_size)
-        self.client = tasks_v2.CloudTasksClient()
+def read_urls_from_file(bucket_name, file_name):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    content = blob.download_as_text()
+    urls = content.splitlines()
+    return urls
 
-    def process_files(self):
-        # get all text files in the directory
-        files = [f for f in os.listdir(self.folder_path) if f.endswith('.txt')]
 
-        # iterate over the files and read them in chunks
-        for file in files:
-            with open(os.path.join(self.folder_path, file), 'r') as f:
-                # save the first line as the rate limit for this site
-                self.rate_limits[file] = int(f.readline().strip())
-                # initialize the last scheduled time to now
-                self.last_scheduled_times[file] = datetime.utcnow()
+def create_task(project_id, location, queue_id, url):
+    client = tasks.CloudTasksClient()
+    parent = client.queue_path(project_id, location, queue_id)
 
-                # parent is the queue that will process the task
-                parent = self.client.queue_path('project_id', 'location_id', 'queue_id')
+    task = {
+        "http_request": {
+            "http_method": tasks.HttpMethod.POST,
+            "url": url
+        }
+    }
 
-                # now process the rest of the file in chunks
-                for piece in self.read_in_chunks(f):
-                    df = pd.DataFrame(piece, columns=['Product_URL'])
-                    for index, row in df.iterrows():
-                        url = row['Product_URL']
+    response = client.create_task(request={"parent": parent, "task": task})
+    print(f"Task created: {response.name}")
 
-                        # construct the request body
-                        task = {
-                            'app_engine_http_request': {  # Specify the type of request.
-                                'http_method': 'POST',
-                                'relative_uri': '/example_task_handler',  # Specify the handler.
-                                'body': url.encode(),  # Send the URL in the body.
-                                'headers': {
-                                    'Content-Type': 'text/plain',
-                                },
-                            },
-                        }
 
-                        # Convert "seconds from now" into an rfc3339 datetime string.
-                        d = self.last_scheduled_times[file] + timedelta(seconds=1/self.rate_limits[file])
-                        timestamp = timestamp_pb2.Timestamp()
-                        timestamp.FromDatetime(d)
+if __name__ == "__main__":
+    # Define the GCS bucket and text file information
+    bucket_name = "your-bucket-name"
+    file_name = "path/to/your-file.txt"
 
-                        # Add the timestamp to the tasks.
-                        task['schedule_time'] = timestamp
+    # Define the Cloud Tasks information
+    project_id = "your-project-id"
+    queue_id = "your-queue-id"
+    location = "your-location"  # e.g., "us-central1"
 
-                        # Use the client to build and send the task.
-                        response = self.client.create_task(parent, task)
-                        print('Created task {}'.format(response.name))
-
-                        # Update the last scheduled time
-                        self.last_scheduled_times[file] = d
+    urls = read_urls_from_file(bucket_name, file_name)
+    for url in urls:
+        create_task(project_id, location, queue_id, url)
