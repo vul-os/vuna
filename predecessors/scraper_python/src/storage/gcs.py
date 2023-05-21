fb3b675c3d341ecdaff97eb40bb595a33f1d2fd9
Imran Paruk <imranparuk@live.com>
2023-05-21 21:41:18 +0000

    .

--- predecessors/scraper_python/src/storage/gcs.py
+++ predecessors/scraper_python/src/storage/gcs.py
@@ -87,8 +87,9 @@ class StorageUtilsGCS(StorageUtils):
 
     
     def get_latest_files(self, folder_prefix, text_in):
+        prefix = f"{self.bucket_name}/{folder_prefix}" if folder_prefix else None
         # Dictionary to store the latest file for each encoded_site
-        blobs = self.bucket.list_blobs(prefix=f"{self.bucket_name}/{folder_prefix}", delimiter="/")
+        blobs = self.bucket.list_blobs(prefix=prefix, delimiter="/")
         latest_files = {}
         for blob in blobs:
             blob_name = blob.name
@@ -105,7 +106,9 @@ class StorageUtilsGCS(StorageUtils):
     def get_latest_file(self, folder_prefix, text_in):
         latest_filename = None
         latest_datetime = None
-        blobs = self.bucket.list_blobs(prefix=f"{self.bucket_name}/{folder_prefix}", delimiter="/")
+        prefix = f"{self.bucket_name}/{folder_prefix}" if folder_prefix else None
+        # Dictionary to store the latest file for each encoded_site
+        blobs = self.bucket.list_blobs(prefix=prefix, delimiter="/")
         for blob in blobs:
             blob_name = blob.name
             if text_in in blob_name:
