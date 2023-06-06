328886ec0d7a6958e91ba56965e03c87813244b9
exolution technologies <exolutionza@gmail.com>
2023-06-06 10:48:40 +0000

    update to query

--- predecessors/scraper-backend-go/internal/bi/sql/total_value.sql
+++ predecessors/scraper-backend-go/internal/bi/sql/total_value.sql
@@ -1,7 +1,26 @@
-SELECT SUM(PriceStock) AS TotalPriceStock
-FROM (
-  SELECT DISTINCT ProductIdentifier, MAX(DateCreated) AS LatestDate, SUM(Price * MaxQty) AS PriceStock
-  FROM `scraping-is-hard.scrapers.datapoint_raw`
-  WHERE MaxQty > 0
-  GROUP BY ProductIdentifier
-) AS subquery;
+WITH latest_datapoint AS (
+  SELECT
+    ProductIdentifier, 
+    DateCreated, 
+    Price, 
+    MaxQty,
+    ROW_NUMBER() OVER(PARTITION BY ProductIdentifier ORDER BY DateCreated DESC) AS row_num
+  FROM
+    `scraping-is-hard.scrapers.datapoint_raw`
+  WHERE 
+    MaxQty > 0
+), latest_product_info AS (
+  SELECT
+    ProductIdentifier, 
+    DateCreated AS LatestDate, 
+    Price, 
+    MaxQty
+  FROM
+    latest_datapoint
+  WHERE
+    row_num = 1
+)
+SELECT
+  SUM(Price * MaxQty) AS total_sum
+FROM
+  latest_product_info
