400a130d0048d474875b8555f0f628c8b0aa80f1
imranparuk <imranparuk@live.com>
2023-08-24 00:43:22 +0200

    updates

--- predecessors/scraper-go/internal/pkg/utils/encodeCompress.go
+++ predecessors/scraper-go/internal/pkg/utils/encodeCompress.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"bytes"
-	"compress/zlib"
 	"encoding/base64"
 	"fmt"
 	"net/url"
@@ -20,7 +18,7 @@ func GetHostName(rawURL string) (string, string, error) {
 		return "", "", fmt.Errorf("error parsing URL: %v", err)
 	}
 	hostId := RemoveWWWPrefix(RemoveHTTPPrefix(parsedURL.Host))
-	productId := RemoveWWWPrefix(RemoveHTTPPrefix(parsedURL.RawPath))
+	productId := RemoveWWWPrefix(RemoveHTTPPrefix(rawURL))
 	return hostId, productId, nil
 }
 
@@ -37,16 +35,13 @@ func RemoveWWWPrefix(input string) string {
 }
 
 func StringToIdentifier(urlString string, otherStringIds []string) (string, string, error) {
-	// Fixed character combination
 	hostString, productString, err := GetHostName(urlString)
 
 	if err != nil {
 		return "", "", err
 	}
-	hostId, err := StringToSiteIdentifier(hostString)
-	if err != nil {
-		return "", "", err
-	}
+	hostId := StringToSiteIdentifier(hostString)
+
 	productId, err := StringToProductIdentifier(productString, otherStringIds)
 	if err != nil {
 		return "", "", err
@@ -55,93 +50,37 @@ func StringToIdentifier(urlString string, otherStringIds []string) (string, stri
 }
 
 func StringToProductIdentifier(stringy string, otherStringIds []string) (string, error) {
-	// Concatenate the strings with the fixed character combination
+	fmt.Println("here: ", stringy, otherStringIds)
 	stringsToJoin := append(otherStringIds, stringy)
 	joinedString := strings.Join(stringsToJoin, CHAR_COMBO)
 	encoded4URL := url.QueryEscape(joinedString)
-
-	encodedString, err := EncodeAndCompressString(encoded4URL)
-	if err != nil {
-		return "", err
-	}
-	return encodedString, nil
+	fmt.Println("here2: ", encoded4URL)
+	return EncodeString(encoded4URL), nil
 }
 
-func StringToSiteIdentifier(stringy string) (string, error) {
-	encodedHostString, err := EncodeAndCompressString(stringy)
-	if err != nil {
-		return "", err
-	}
-	return encodedHostString, nil
+func StringToSiteIdentifier(stringy string) string {
+	return EncodeString(stringy)
 }
 
 func EncodeURL(urlStr string) string {
-	// URL encoding
 	encodedURL := url.QueryEscape(urlStr)
-	// Base64 encoding
 	encodedURL = base64.URLEncoding.EncodeToString([]byte(encodedURL))
 	return encodedURL
 }
 
+func EncodeString(str string) string {
+	encodedStr := base64.StdEncoding.EncodeToString([]byte(str))
+	return replacePaddingChars(encodedStr)
+}
+
 func replacePaddingChars(encodedString string) string {
-	// Replace '==' with '#~~#'
 	replacedString := strings.Replace(encodedString, "=", EQUALS_REP, -1)
-
-	// Replace '//' with '()!!'
 	replacedString = strings.Replace(replacedString, "/", BACKSLACK_REP, -1)
-
 	return replacedString
 }
 
 func restorePaddingChars(replacedString string) string {
-	// Restore '#~~#' to '=='
 	restoredString := strings.Replace(replacedString, EQUALS_REP, "=", -1)
-
-	// Restore '()!!' to '//'
 	restoredString = strings.Replace(restoredString, BACKSLACK_REP, "/", -1)
-
 	return restoredString
-}
-
-// EncodeAndCompressURL encodes and compresses the given URL.
-func EncodeAndCompressString(str string) (string, error) {
-
-	// Compress the encoded URL
-	var compressedURL bytes.Buffer
-	compressor := zlib.NewWriter(&compressedURL)
-	_, err := compressor.Write([]byte(str))
-	if err != nil {
-		return "", fmt.Errorf("error compressing URL: %w", err)
-	}
-	compressor.Close()
-
-	// Convert the compressed URL to ASCII base64 string
-	asciiURL := base64.StdEncoding.EncodeToString(compressedURL.Bytes())
-	asciiURL = replacePaddingChars(asciiURL)
-	return asciiURL, nil
-}
-
-// DecompressAndDecodeURL decompresses and decodes the given compressed URL.
-func DecompressAndDecodeString(compressedString string) (string, error) {
-	// Convert the ASCII base64 string to compressed byte slice
-	compressedBytes, err := base64.StdEncoding.DecodeString(compressedString)
-	if err != nil {
-		return "", fmt.Errorf("error decoding base64 URL: %w", err)
-	}
-
-	// Decompress the compressed URL
-	decompressedURL, err := zlib.NewReader(bytes.NewReader(compressedBytes))
-	if err != nil {
-		return "", fmt.Errorf("error decompressing URL: %w", err)
-	}
-	defer decompressedURL.Close()
-
-	// Decode the URL
-	var decompressedBuffer bytes.Buffer
-	_, err = decompressedBuffer.ReadFrom(decompressedURL)
-	if err != nil {
-		return "", fmt.Errorf("error reading decompressed URL: %w", err)
-	}
-	res := restorePaddingChars(decompressedBuffer.String())
-	return res, nil
 }
